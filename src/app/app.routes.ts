5344ffdf650b19fdcdfda02d648a66562339ef89
front3-abudiyab <front3.abudiyab@gmail.com>
2024-01-08 09:46:58 +0200

    create navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { DishesComponent } from './components/pages/dishes/dishes.component';
+import { GalleryComponent } from './components/pages/gallery/gallery.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'dishes', component: DishesComponent, pathMatch: 'full' },
+    { path: 'gallery', component: GalleryComponent, pathMatch: 'full' },
+    { path: 'about-us', component: AboutComponent, pathMatch: 'full' },
+];
